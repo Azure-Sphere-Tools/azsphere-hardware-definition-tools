03a4b6ac6b1b224f07b905277fe1e9dbce35ce35
Omar Beyhum <omar.beyhum@gmail.com>
2021-06-26 03:14:40 +0100

    [AST-21] Add Visual Studio project

---
+++ visualstudio-extension/AZSphereHardwareDefinitionTools/HardwareDefinitionLanguageClient.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Threading;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AZSphereHardwareDefinitionTools
+{
+    [ContentType("json")]
+    [ContentType("cmake")]
+    [Export(typeof(ILanguageClient))]
+    public class HardwareDefinitionLanguageClient : ILanguageClient
+    {
+        public string Name => "AZ Sphere Hardware Definition Tools";
+
+        public IEnumerable<string> ConfigurationSections => new[] { "AzureSphere" };
+
+        public object InitializationOptions => null;
+
+        public IEnumerable<string> FilesToWatch => new[] { "*.json", "CMakeLists.txt" };
+
+        public event AsyncEventHandler<EventArgs> StartAsync;
+        public event AsyncEventHandler<EventArgs> StopAsync;
+
+        public async Task<Connection> ActivateAsync(CancellationToken token)
+        {
+            // call Task.Yield() to force extension activation to run asynchronously and avoid risk of blocking main thread
+            await Task.Yield();
+
+
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "node";
+            info.Arguments = languageServerArgs();
+            info.RedirectStandardInput = true;
+            info.RedirectStandardOutput = true;
+            info.UseShellExecute = false;
+            info.CreateNoWindow = true;
+
+            Process process = new Process();
+            process.StartInfo = info;
+
+            if (process.Start())
+            {
+                return new Connection(process.StandardOutput.BaseStream, process.StandardInput.BaseStream);
+            }
+
+            return null;
+        }
+
+        private static string languageServerArgs()
+        {
+#if DEBUG
+            string workingDirectory = Environment.CurrentDirectory;
+            string extensionDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+            var languageServerEntrypoint = Path.GetFullPath(Path.Combine(extensionDirectory, "..", "..", "server", "dist", "server.js"));
+#else
+            string extensionDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var languageServerEntrypoint = Path.Combine(extensionDirectory, "EmbeddedLanguageServer", "node_modules", "azsphere-hardware-definition-language-server", "dist", "server.js");
+#endif
+            var debugArgs = "--nolazy --inspect=16009";
+            return $"{debugArgs} \"{languageServerEntrypoint}\" --stdio --clientProcessId={Process.GetCurrentProcess().Id}";
+        }
+
+        public async Task OnLoadedAsync()
+        {
+            await StartAsync.InvokeAsync(this, EventArgs.Empty);
+        }
+
+        public Task OnServerInitializeFailedAsync(Exception e)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnServerInitializedAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ visualstudio-extension/AZSphereHardwareDefinitionTools/HardwareDefinitionLanguageContent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace AZSphereHardwareDefinitionTools
+{
+    public class HardwareDefinitionLanguageContent
+    {
+        [Export]
+        [Name("json")]
+        [BaseDefinition(CodeRemoteContentDefinition.CodeRemoteContentTypeName)]
+        internal static ContentTypeDefinition HardwareDefinitionContentType;
+
+        [Export]
+        [FileExtension(".json")]
+        [ContentType("json")]
+        internal static FileExtensionToContentTypeDefinition HardwareDefinitionFileExtension;
+
+
+        [Export]
+        [Name("cmake")]
+        [BaseDefinition(CodeRemoteContentDefinition.CodeRemoteContentTypeName)]
+        internal static ContentTypeDefinition CMakeListsContentType;
+
+        [Export]
+        [FileExtension("CMakeLists.txt")]
+        [ContentType("cmake")]
+        internal static FileExtensionToContentTypeDefinition CMakeListsFileExtension;
+    }
+}
