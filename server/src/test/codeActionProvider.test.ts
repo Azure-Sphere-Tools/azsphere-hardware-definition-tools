68a461160bc893997d309bce2bc2eb2b11acf067
Jiachen <chenstar1357@gmail.com>
2021-07-20 09:54:19 +0100

    finish all test

--- server/src/test/codeActionProvider.test.ts
+++ server/src/test/codeActionProvider.test.ts
@@ -36,49 +36,49 @@ suite("findPinMappingRange", () => {
 
 suite("quickfix", () => {
   teardown(mockfs.restore);
-  // test("Delete the Duplicate pin mapping", () => {
-  //   const gpioPin = new PinMapping("GPIO0", "Gpio", undefined, 0, range(0, 0, 0, 5));
-  //   const importedhwDefFilePath = "my_app/hardwareDef.json";
-  //   const importedhwDefinitionFile = new HardwareDefinition(asURI(importedhwDefFilePath), undefined, [gpioPin]);
+  test("Delete the Duplicate pin mapping", () => {
+    const gpioPin = new PinMapping("GPIO0", "Gpio", undefined, 0, range(0, 0, 0, 5));
+    const importedhwDefFilePath = "my_app/hardwareDef.json";
+    const importedhwDefinitionFile = new HardwareDefinition(asURI(importedhwDefFilePath), undefined, [gpioPin]);
 
-  //   const hwDefFilePath = "my_app/ODM.json";
-  //   const validPin1 = new PinMapping("LED_RED", "Gpio", "GPIO0", undefined, range(0, 0, 0, 7));
-  //   const validPin2 = new PinMapping("LED_BLUE", "Gpio", "GPIO0", undefined, range(1, 0, 1, 7));
-  //   validPin1.mappingPropertyRange = range(0,2,0,3);
-  //   validPin2.mappingPropertyRange = range(1,2,1,3);
-  //   const hwDefinitionFile = new HardwareDefinition(asURI(hwDefFilePath), undefined, [validPin1,validPin2], [importedhwDefinitionFile]);
+    const hwDefFilePath = "my_app/ODM.json";
+    const validPin1 = new PinMapping("LED_RED", "Gpio", "GPIO0", undefined, range(0, 0, 0, 7));
+    const validPin2 = new PinMapping("LED_BLUE", "Gpio", "GPIO0", undefined, range(1, 0, 1, 7));
+    validPin1.mappingPropertyRange = range(0,2,0,3);
+    validPin2.mappingPropertyRange = range(1,2,1,3);
+    const hwDefinitionFile = new HardwareDefinition(asURI(hwDefFilePath), undefined, [validPin1,validPin2], [importedhwDefinitionFile]);
 
-  //   const textDocument = {
-  //     uri: asURI(hwDefFilePath),
-  //     languageId: "",
-  //     version: 0,
-  //     getText: function (range?: Range): string {
-  //       throw new Error("Function not implemented.");
-  //     },
-  //     positionAt: function (offset: number): Position {
-  //       throw new Error("Function not implemented.");
-  //     },
-  //     offsetAt: function (position: Position): number {
-  //       throw new Error("Function not implemented.");
-  //     },
-  //     lineCount: 0
-  //   };
+    const textDocument = {
+      uri: asURI(hwDefFilePath),
+      languageId: "",
+      version: 0,
+      getText: function (range?: Range): string {
+        return "{{'Name': 'LED_RED', 'Type': 'Gpio', 'Mapping': 'GPIO0'},{'Name': 'LED_BLUE', 'Type': 'Gpio', 'Mapping': 'GPIO0'}}";
+      },
+      positionAt: function (offset: number): Position {
+        throw new Error("Function not implemented.");
+      },
+      offsetAt: function (position: Position): number {
+        throw new Error("Function not implemented.");
+      },
+      lineCount: 0
+    };
 
-  //   const text = textDocument.getText();
-  //   const diagnostics: Diagnostic[] = findDuplicateMappings(hwDefinitionFile, text, textDocument, false);
+    const text = textDocument.getText();
+    const diagnostics: Diagnostic[] = findDuplicateMappings(hwDefinitionFile, text, textDocument, false);
     
-  //   const params: CodeActionParams = {
-  //     context: {diagnostics: diagnostics},
-  //     range: {
-  //       start: {character: 1, line: 0},
-  //       end: {character: 0, line: 0}
-  //     },
-  //     textDocument: {uri: asURI(hwDefFilePath)}
-  //   };
-  //   const codeAction = quickfix(hwDefinitionFile,params);
-  //   assert.strictEqual(codeAction[0].title, "Delete the Duplicate pin mapping");
+    const params: CodeActionParams = {
+      context: {diagnostics: diagnostics},
+      range: {
+        start: {character: 0, line: 0},
+        end: {character: 1, line: 0}
+      },
+      textDocument: {uri: asURI(hwDefFilePath)}
+    };
+    const codeAction = quickfix(hwDefinitionFile,params);
+    assert.strictEqual(codeAction[0].title, "Delete the Duplicate pin mapping");
 
-  // });
+  });
 
   test("Delete the Invalid pin mapping", () => {
     const gpioPin = new PinMapping("GPIO0", "Gpio", undefined, 0, range(0, 0, 0, 5));
@@ -96,8 +96,8 @@ suite("quickfix", () => {
     const params: CodeActionParams = {
       context: {diagnostics: warningDiagnostics},
       range: {
-        start: {character: 1, line: 0},
-        end: {character: 0, line: 0}
+        start: {character: 0, line: 0},
+        end: {character: 1, line: 0}
       },
       textDocument: {uri: asURI(hwDefFilePath)}
     };
@@ -143,8 +143,8 @@ suite("quickfix", () => {
     const params: CodeActionParams = {
       context: {diagnostics: warningDiagnostics},
       range: {
-        start: {character: 1, line: 0},
-        end: {character: 0, line: 0}
+        start: {character: 0, line: 0},
+        end: {character: 1, line: 0}
       },
       textDocument: {uri: asURI(hwDefFilePath)}
     };
