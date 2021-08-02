ac7838762bd17a1b7834fcfe1596d4be0858d758
Dorin Botan <dorinbotan@mail.ru>
2021-08-03 00:33:16 +0100

    Added VSCode E2E tests for Unknown Import

--- vscode-extension/src/test/diagnostics.test.ts
+++ vscode-extension/src/test/diagnostics.test.ts
@@ -1,10 +1,10 @@
 import * as vscode from 'vscode';
 import * as assert from 'assert';
 import { getDocUri, activate } from './helper';
+import * as path from 'path';
 
 suite('Should get diagnostics', () => {
 	const docUri = getDocUri('diagnostics.json');
-
 	test('Diagnoses duplicate and non-existent mappings', async () => {
 		await testDiagnostics(docUri, [
 			{ message: 'Peripheral USER_BUTTON_A not found.', range: toRange(13, 17, 13, 32), severity: vscode.DiagnosticSeverity.Error, source: 'az sphere' },
@@ -20,6 +20,18 @@ suite('Should get diagnostics', () => {
 		await testDiagnostics(pinBlockDocUri, [
 			{ message: 'MT3620_RDB_PWM_CONTROLLER0 configured as Gpio by MT3620_RDB_LED1_BLUE', range: toRange(25, 6, 29, 7), severity: vscode.DiagnosticSeverity.Warning, source: 'az sphere' },
 			{ message: 'MT3620_RDB_ISU0_SPI configured as I2cMaster by MT3620_RDB_ISU0_I2C', range: toRange(35, 6, 39, 7), severity: vscode.DiagnosticSeverity.Warning, source: 'az sphere' }
+		]);
+	});
+
+	const unknownImportUri = getDocUri('unknownImport.json');
+	test('Unknown Import', async () => {
+		const hwDefinitionFilePath = path.dirname(unknownImportUri.fsPath);
+		const SdkPath = process.platform == "linux" 
+			? "/opt/azurespheresdk" 
+			: "C:\\Program Files (x86)\\Microsoft Azure Sphere SDK";
+
+		await testDiagnostics(unknownImportUri, [
+			{ message: `Cannot find 'nonexistent.json' under ${hwDefinitionFilePath} or ${SdkPath}.`, range: toRange(14, 14, 14, 32), severity: vscode.DiagnosticSeverity.Warning, source: 'az sphere' },
 		]);
 	});
 });
