fa11bbbe19f847b16cbecbb7fcd2a64e44c61da3
Omar Beyhum <omar.beyhum@gmail.com>
2021-08-02 11:16:23 +0100

    Add code coverage

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -7,6 +7,9 @@ set -e
 # TODO remove 'LANGUAGE_SERVER_MODE=TEST' when connection issue fixed in server.ts
 LANGUAGE_SERVER_MODE=TEST ./node_modules/mocha/bin/mocha --ui tdd --color --timeout 10s server/dist/test/**.test.js
 
+if [ $1 == "ONLY_UNIT_TESTS" ]; then
+  exit 0
+fi
 # Run e2e tests
 export CODE_TESTS_PATH="$(pwd)/vscode-extension/dist/test"
 export CODE_TESTS_WORKSPACE="$(pwd)/testFixture"
