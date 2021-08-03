3f2f2735d729ece543ad95f75750091ff1aaead1
Dino <dinohossain1@gmail.com>
2021-08-03 02:08:12 +0100

    [AST-61] - Refactoring + Started tests

--- server/src/hardwareDefinitionHeaderGeneration.ts
+++ server/src/hardwareDefinitionHeaderGeneration.ts
@@ -6,15 +6,26 @@ import { connection, validateDocument } from "./server";
 
 
 
-export const hwDefinitionHeaderGen = async(textDocument: TextDocument) => {
-const hwDefintionUri = await validateDocument(textDocument);
-
+export const hwDefinitionHeaderGen = async(textDocument: TextDocument): Promise<string | undefined> => {
+const validateHardwareDefinition = await validateDocument(textDocument);
     // Hardware Definition header generation
-    if (hwDefintionUri) {
-      exec(`azsphere hardware-definition generate-header --hardware-definition-file ${URI.parse(hwDefintionUri).fsPath}`, (err, stdout, stderr) => {
-        if (err) return connection.sendNotification(ShowMessageNotification.type, { message: `Header file generation error: ${err.message}`, type: MessageType.Error });
-        if (stderr) return connection.sendNotification(ShowMessageNotification.type, { message: `Header file generation stderr: ${stderr}`, type: MessageType.Error });
-        if (stdout) return connection.sendNotification(ShowMessageNotification.type, { message: stdout, type: MessageType.Info });
+    if (validateHardwareDefinition) {
+      const lol = URI.parse(validateHardwareDefinition).fsPath;
+      exec(`azsphere hardware-definition generate-header --hardware-definition-file ${URI.parse(validateHardwareDefinition).fsPath}`, (err, stdout, stderr) => {
+        if (err) {
+          connection.sendNotification(ShowMessageNotification.type, { message: `Header file generation error: ${err.message}`, type: MessageType.Error });
+          return err.message;
+      } 
+        if (stderr) { 
+          connection.sendNotification(ShowMessageNotification.type, { message: `Header file generation stderr: ${stderr}`, type: MessageType.Error });
+          return stderr;
+        }
+
+        if (stdout) { 
+          connection.sendNotification(ShowMessageNotification.type, { message: stdout, type: MessageType.Info });
+          return stdout;
+        }
       });
     }
+    return;
 };
