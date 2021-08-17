ca6ad605045ef1c79b8954db7c3664875c058aa1
Dino <dinohossain1@gmail.com>
2021-08-17 14:12:55 +0100

    [AST-67] - Access all cmakecaches under /out/ path

--- server/src/cMakeLists.ts
+++ server/src/cMakeLists.ts
@@ -3,6 +3,7 @@ import * as path from "path";
 import { URI } from "vscode-uri";
 import { asURI } from "./test/testUtils";
 import { Logger } from "./utils";
+import * as glob from "glob";
 
 export function parseCommandsParams(CMakeListsPath: string, logger: Logger = console): string | undefined {
   try {
@@ -14,11 +15,11 @@ export function parseCommandsParams(CMakeListsPath: string, logger: Logger = con
     if (match[1].length && match[2].length) {
       let [dir, file]: string[] = [match[1], match[2]];
       // TODO: Find out/*/cmakecache
-      const absolutePath: string = path.resolve(path.join(path.dirname(URI.parse(asURI(CMakeListsPath)).fsPath), "out/ARM-Debug/"));
+      // const absolutePath: string = path.resolve(path.join(path.dirname(URI.parse(asURI(CMakeListsPath)).fsPath), "out/ARM-Debug/"));
 
       // Check if wrapped with ${}, not hardcoded
       if (/\${.*?\}/g.test(dir)) {
-        const cacheTxt: string | undefined = getCacheTxt(absolutePath);
+        const cacheTxt: string | undefined = getCacheTxt(CMakeListsPath);
 
         if (cacheTxt) {
           const TARGET_DIRECTORY = getHwDefPathFromCache(dir, cacheTxt);
@@ -27,8 +28,7 @@ export function parseCommandsParams(CMakeListsPath: string, logger: Logger = con
       }
 
       if (/\${.*?\}/g.test(file)) {
-        const cacheTxt: string | undefined = getCacheTxt(absolutePath);
-
+        const cacheTxt: string | undefined = getCacheTxt(CMakeListsPath);
         if (cacheTxt) {
           const TARGET_DEFINITION = getHwDefPathFromCache(file, cacheTxt);
           TARGET_DEFINITION ? (file = TARGET_DEFINITION) : undefined;
@@ -51,15 +51,21 @@ export function parseCommandsParams(CMakeListsPath: string, logger: Logger = con
 }
 
 const getCacheTxt = (absolutePath: string): string => {
-  return fs.readFileSync(path.join(absolutePath, getMostRecentFile(absolutePath))).toString();
+  return fs.readFileSync(getMostRecentFile(absolutePath)).toString();
 };
 
+/**
+ *
+ * @param targetValue TARGET_DIRECTORY or TARGET_DEFINITION value parsed from CMakeLists.txt
+ * @param cacheTxt CMakeCache.txt text
+ * @returns Correctly formatted dir/file value
+ */
 const getHwDefPathFromCache = (targetValue: string, cacheTxt: string) => {
+  // "${file.json}" -> "file.json"
   const variableWithoutWrapping: RegExpExecArray | null = /(?<=\{)(.*?)(?=\})/s.exec(targetValue);
   if (variableWithoutWrapping && cacheTxt) {
     // Match the variable line in the cache text
     const regex: RegExp | null = new RegExp(`${variableWithoutWrapping[0]}(.*)`, "g");
-
     const variableLineInCache: RegExpExecArray | null = regex.exec(cacheTxt);
     if (variableLineInCache) {
       // Match everything after "="
@@ -71,13 +77,10 @@ const getHwDefPathFromCache = (targetValue: string, cacheTxt: string) => {
 
 const getMostRecentFile = (dir: string) => {
   const files = orderReccentFiles(dir);
-  return files.length ? files[0].file : "";
+  return files?.length ? files[0].file : "";
 };
 
-const orderReccentFiles = (dir: string) => {
-  return fs
-    .readdirSync(dir)
-    .filter((file) => fs.lstatSync(path.join(dir, file)).isFile() && file.includes("CMakeCache.txt"))
-    .map((file) => ({ file, mtime: fs.lstatSync(path.join(dir, file)).mtime }))
-    .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
+const orderReccentFiles = (dir: string): { file: string; mtime: Date }[] | undefined => {
+  const files = glob.sync(`${path.resolve(path.dirname(URI.parse(asURI(dir)).fsPath))}/out/**/CMakeCache.txt`);
+  return files.map((file) => ({ file, mtime: fs.lstatSync(path.join(file)).mtime })).sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
 };
