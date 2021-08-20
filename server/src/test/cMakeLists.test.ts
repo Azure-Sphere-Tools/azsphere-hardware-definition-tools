d7b052c4fd3f5a215a75fae05f4bd559aa645f0b
Dino <dinohossain1@gmail.com>
2021-08-20 17:54:17 +0100

    Get hardware definition path from CMake cache if not hardcoded in CMakeLists.txt (#47)
    * [AST-67] - Get hardware definition path from CMake cache if not hardcoded in CMakeLists.txt
    
    * [AST-67] - Refactoring
    
    * [AST-67] - Access all cmakecaches under /out/ path
    
    * [AST-67] - Added tests
    
    * [AST-67] - Added more tests
    
    * [AST-67] - Get hardware definition path from CMake cache if not hardcoded in CMakeLists.txt
    
    [AST-67] - Refactoring
    
    [AST-67] - Access all cmakecaches under /out/ path
    
    [AST-67] - Added tests
    
    [AST-67] - Added more tests
    
    * [AST-67] - Fix dependencies
    
    * [AST-67] - Fix test
    
    * Fix package.json files
    
    * [AST-67] - Added unhandled case: CMakeListsis not cached under the /out/ directory
    
    Co-authored-by: Omar Beyhum <omar.beyhum@gmail.com>

--- server/src/test/cMakeLists.test.ts
+++ server/src/test/cMakeLists.test.ts
@@ -1,44 +1,67 @@
-import { parseCommandsParams } from "../cMakeLists";
+import { parseCommandsParams, getCacheTxt, getMostRecentFile, orderReccentFiles } from "../cMakeLists";
 import * as assert from "assert";
 import * as mockfs from "mock-fs";
 import * as path from "path";
 
 suite("CMakeLists Infer", () => {
+  const cacheTxt = `#######################
+  # EXTERNAL cache entries
+  #######################
+  //VarA comment
+  VAR_A:STRING=HardwareDefinitions/mt3620_rdb
+  //VarB comment
+  VAR_B:STRING=template_appliance.json`;
+
+  const obj = {
+    "my_app/CMakeListsBothStrings.txt": `azsphere_target_hardware_definition(\${PROJECT_NAME} TARGET_DIRECTORY "HardwareDefinitions/mt3620_rdb" TARGET_DEFINITION "template_appliance.json")`,
+    "my_app/CMakeListsBothVars.txt": `azsphere_target_hardware_definition(\${PROJECT_NAME} TARGET_DIRECTORY "\${VAR_A}" TARGET_DEFINITION "\${VAR_B}"`,
+    "my_app/CMakeListsDirStringDefVar.txt": `azsphere_target_hardware_definition(\${PROJECT_NAME} TARGET_DIRECTORY "HardwareDefinitions/mt3620_rdb" TARGET_DEFINITION "\${VAR_B}")`,
+    "my_app/CMakeListsDirVarDefString.txt": `azsphere_target_hardware_definition(\${PROJECT_NAME} TARGET_DIRECTORY "\${VAR_A}" TARGET_DEFINITION "template_appliance.json")`,
+
+    "my_app/out/a/CMakeCache.txt": cacheTxt,
+    "my_app/out/a/b/CMakeCache.txt": cacheTxt,
+
+    "my_app/HardwareDefinitions/mt3620_rdb/template_appliance.json": "file_content",
+  };
+  setup(() => {
+    mockfs(obj);
+  });
+  teardown(mockfs.restore);
+
   test("Azuresphere Target Hardware Definition specified in CMakeLists", () => {
-    mockfs({
-      my_application: {
-        "CMakeLists.txt": `
-			cmake_minimum_required (VERSION 3.10)
-			
-			project (Blink C)
-			
-			azsphere_configure_tools(TOOLS_REVISION "21.04")
-			azsphere_configure_api(TARGET_API_SET "9")
-			find_path(
-				TARGET_DIRECTORY
-				NAMES "myfile.txt"
-				PATHS "HardwareDefinitions" "./"
-				NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
-			# Create executable
-			add_executable (\${PROJECT_NAME} main.c)
-			target_link_libraries (\${PROJECT_NAME} applibs pthread gcc_s c)
-			azsphere_target_hardware_definition(\${PROJECT_NAME} TARGET_DIRECTORY "HardwareDefinitions/" TARGET_DEFINITION "template_appliance.json")
-			
-			azsphere_target_add_image_package(\${PROJECT_NAME})
-			
-			message("LOOK HERE \${TARGET_DIRECTORY}")`,
-      },
-      "my_application/HardwareDefinitions/template_appliance.json": "file_content",
-    });
-    const CMAKEListsPath = "my_application/CMakeLists.txt";
+    const CMakeListsBothStrings: string | undefined = parseCommandsParams(path.resolve("my_app/CMakeListsBothStrings.txt"));
+    const CMakeListsBothVars: string | undefined = parseCommandsParams(path.resolve("my_app/CMakeListsBothVars.txt"));
+    const CMakeListsDirStringDefVar: string | undefined = parseCommandsParams(path.resolve("my_app/CMakeListsDirStringDefVar.txt"));
+    const CMakeListsDirVarDefString: string | undefined = parseCommandsParams(path.resolve("my_app/CMakeListsDirVarDefString.txt"));
 
-    const fullPath: string | undefined = parseCommandsParams(path.resolve(CMAKEListsPath));
+    const expectedPath = "my_app/HardwareDefinitions/mt3620_rdb/template_appliance.json";
 
-    const expectedPath = "my_application/HardwareDefinitions/template_appliance.json";
-    if (fullPath) {
-      assert.strictEqual(path.resolve(fullPath), path.resolve(expectedPath));
-    } else {
-      assert.fail(`Path was undefined`);
-    }
+    CMakeListsBothStrings ? assert.strictEqual(path.resolve(CMakeListsBothStrings), path.resolve(expectedPath)) : assert.fail(`Path was undefined`);
+    CMakeListsBothVars ? assert.strictEqual(path.resolve(CMakeListsBothVars), path.resolve(expectedPath)) : assert.fail(`Path was undefined`);
+    CMakeListsDirStringDefVar ? assert.strictEqual(path.resolve(CMakeListsDirStringDefVar), path.resolve(expectedPath)) : assert.fail(`Path was undefined`);
+    CMakeListsDirVarDefString ? assert.strictEqual(path.resolve(CMakeListsDirVarDefString), path.resolve(expectedPath)) : assert.fail(`Path was undefined`);
+  });
+
+  test("Get CMakeLists cache text", () => {
+    const actualCacheTxt: string | undefined = getCacheTxt("my_app/CMakeListsDirStringDefVar.txt");
+    actualCacheTxt ? assert.strictEqual(actualCacheTxt, cacheTxt) : assert.fail(`Failed to read CMakeLists cache`);
+  });
+
+  test("Returns undefined if no CMakeCache.txt is found", () => {
+    const actualCacheTxt = getCacheTxt("my_app/random/path/withNoCache/CMakeCache.txt");
+    console.log(actualCacheTxt);
+    !actualCacheTxt ? assert.strictEqual(actualCacheTxt, undefined) : assert.fail(`getCacheTxt() did not return undefined`);
+  });
+
+  test("Get all CMakeLists cache text under /out/", () => {
+    const actualFiles = orderReccentFiles("my_app/CMakeListsDirStringDefVar.txt");
+    // There should be 2 caches
+    actualFiles ? assert.strictEqual(actualFiles.length, 2) : assert.fail(`Did not get all CMakeLists cache text under /out/`);
+  });
+
+  test("Get most recent CMakeCache.txt", () => {
+    mockfs({ "my_app/out/newer/CMakeCache.txt": mockfs.file({ mtime: new Date(1) }) });
+    const mostRecentFile = getMostRecentFile("my_app/CMakeListsDirStringDefVar.txt");
+    mostRecentFile ? assert.strictEqual(mostRecentFile.endsWith("my_app/out/newer/CMakeCache.txt"), true) : assert.fail(`Did not get most reccent CMakeCache.txt`);
   });
 });
